ce9e411f01a6e75a1263be5a76a7a76245e922ff
Hiruthik J <hiruthik.j@chargebee.com>
2022-02-07 19:21:14 +0530

    fix: Spacing, Alignment of Delete Modal

--- src/components/DeleteMember.vue
+++ src/components/DeleteMember.vue
@@ -1,45 +1,37 @@
 <template>
   <div id="modal" class="modal is-active">
     <div class="modal-background" @click="$emit('closeDeleteModal')"></div>
+    <button
+      class="modal-close is-large"
+      aria-label="close"
+      @click="$emit('closeDeleteModal')"
+    ></button>
     <div class="modal-content">
       <div class="box">
-        <article class="media">
-          <div class="media-left">
-            <figure class="image is-64x64">
-              <i class="fas fa-info-circle" id="info-icon"></i>
-            </figure>
-          </div>
-          <div class="media-content">
-            <button
-              class="modal-close is-large"
-              aria-label="close"
-              @click="$emit('closeDeleteModal')"
-            ></button>
-            <div class="content">
-              <p>
-                <strong
-                  >{{ email }} will not be able to access the site.</strong
-                >
-              </p>
-              <div class="is-text-centered">
-                <button
-                  class="button is-danger"
-                  type="submit"
-                  @click="callDeleteMember"
-                >
-                  Delete
-                </button>
-                <button
-                  class="button"
-                  type="button"
-                  @click="$emit('closeDeleteModal')"
-                >
-                  Cancel
-                </button>
-              </div>
-            </div>
-          </div>
-        </article>
+        <figure
+          class="image is-64x64 is-pulled-left has-text-centered is-vcentered"
+        >
+          <i class="fas fa-info-circle" id="info-icon"></i>
+        </figure>
+        <p class="has-text-centered delete-warning">
+          <strong>{{ email }} will not be able to access the site.</strong>
+        </p>
+        <div class="has-text-centered">
+          <button
+            class="button is-danger"
+            type="submit"
+            @click="callDeleteMember"
+          >
+            Delete
+          </button>
+          <button
+            class="button"
+            type="button"
+            @click="$emit('closeDeleteModal')"
+          >
+            Cancel
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -70,11 +62,14 @@ export default {
 
 <style scoped>
 #info-icon {
-  font-size: 30px;
+  font-size: 3rem;
   color: orange;
 }
 button {
   margin-left: 10px;
+}
+.delete-warning {
+  margin-bottom: 1rem;
 }
 </style>
 
